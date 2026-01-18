31022bc99cf98c982372ebdac1e693692a68f15c
xymvl4t3d <hanif96@outlook.com>
2026-01-19 01:48:04 +0800

    Changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Post-boot setup
-ujust enable-flatpak
-flatpak install flathub com.visualstudio.code com.rustdesk.RustDesk
-ujust distrobox-create --image ubuntu:24.04 devops-workspace
+flatpak install -y flathub com.visualstudio.code com.rustdesk.RustDesk
 echo "Setup complete! Reboot optional."
