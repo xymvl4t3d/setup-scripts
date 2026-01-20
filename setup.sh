9e51e5bbb1c582e87ba80852cb0562ce0930c050
xymvl4t3d <hanif96@outlook.com>
2026-01-20 19:04:29 +0800

    Changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Post-boot setup
-flatpak install -y flathub com.visualstudio.code com.rustdesk.RustDesk devpod
+flatpak install -y flathub com.visualstudio.code com.rustdesk.RustDesk devpod org.videolan.VLC
