0aa82731d1fdcfb86e4c1040f4ed4dfe482ce91a
xymvl4t3d <hanif96@outlook.com>
2026-02-08 21:25:18 +0800

    Changed .gitignore and setup.sh, added .editorconfig

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Post-boot setup
-flatpak install -y flathub com.visualstudio.code com.rustdesk.RustDesk devpod org.videolan.VLC io.mpv.Mpv com.github.iwalton3.jellyfin-media-player io.github.mfat.sshpilot
+flatpak install -y flathub com.visualstudio.code \
+	com.rustdesk.RustDesk \
+	devpod org.videolan.VLC \
+	io.mpv.Mpv \
+	com.github.iwalton3.jellyfin-media-player \
+	io.github.mfat.sshpilot \
+	md.obsidian.Obsidian \
+	com.slack.Slack \
+	com.discordapp.Discord
